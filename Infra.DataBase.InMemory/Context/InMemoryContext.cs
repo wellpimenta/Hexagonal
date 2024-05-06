6152c6663536286aa5d8ef5b640ea8bae28b2d6e
wellpimenta <wellpimenta@yahoo.com.br>
2024-05-06 11:52:41 -0300

    Infra

---
+++ Infra.DataBase.InMemory/Context/InMemoryContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra.DataBase.InMemory.Context
+{
+    public class InMemoryContext
+    {
+        //TODO -> implementar a base de dados in memory
+    }
+}
